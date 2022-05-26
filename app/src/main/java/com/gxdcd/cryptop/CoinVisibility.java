5e867333de78da889a41e58867507493bd0cd680
gxdcd <105979959+gxdcd@users.noreply.github.com>
2022-05-26 21:00:34 +0500

    Update CoinVisibility.java

--- app/src/main/java/com/gxdcd/cryptop/CoinVisibility.java
+++ app/src/main/java/com/gxdcd/cryptop/CoinVisibility.java
@@ -5,15 +5,22 @@ import com.gxdcd.cryptop.api.cmc.CmcItem;
 enum CoinVisibility {
     ALL,
     STABLECOINS,
-    NO_STABLECOINS;
+    NO_STABLECOINS,
+    FAVORITES;
 
-    public boolean willShow(CmcItem item) {
+    public boolean willShow(CmcItem item, IsFavorite isFavorite) {
         switch (this) {
             case STABLECOINS:
                 return item.isStablecoin();
             case NO_STABLECOINS:
                 return !item.isStablecoin();
+            case FAVORITES:
+                return isFavorite.getIsFavorite(item.id);
         }
         return true;
+    }
+
+    public interface IsFavorite {
+        boolean getIsFavorite(Integer id);
     }
 }
