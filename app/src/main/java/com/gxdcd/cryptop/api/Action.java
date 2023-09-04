af079ff1ac2b0d3ab1dcae02d61ef04f2637d3e5
DenisovMihail <123821433+DenisovMihail@users.noreply.github.com>
2023-09-04 15:48:03 +0300

    Update Action.java

--- app/src/main/java/com/gxdcd/cryptop/api/Action.java
+++ app/src/main/java/com/gxdcd/cryptop/api/Action.java
@@ -2,6 +2,7 @@ package com.gxdcd.cryptop.api;
 
 import android.graphics.Color;
 
+
 // Интерфейс используемый для функции обратного вызова
 // в асинхронных задачах, вызываемой после завершения задачи
 // (functional interface)
